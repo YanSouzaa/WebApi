5382684a9f11035b255e5fdd8b091576b70b04f5
yan.leite <yan.leite@delage.com.br>
2024-03-17 19:44:54 -0300

    Implementar Code First para criacao do banco de dados
    - Configura a classe de contexto para se conectar ao banco de dados e definir as entidades.
    - Define as propriedades e configuracoes das entidades necessarias para criar as tabelas no banco de dados.
    - Utiliza as migracoes do Entity Framework Core para criar ou atualizar o esquema do banco de dados com base nas alteracoes no modelo.

---
+++ Products.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Products_Data
+{
+    public class ConnectionContextFactory : IDesignTimeDbContextFactory<ConnectionContext>
+    {
+        public ConnectionContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ConnectionContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new ConnectionContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Products.Data/Migrations/20240317222031_Products.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Products_Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Products : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Stock = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Value = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "Id", "Name", "Stock", "Value" },
+                values: new object[,]
+                {
+                    { 1, "Abacate", 50m, 5m },
+                    { 2, "Biscoito", 100m, 5m },
+                    { 3, "Manga", 80m, 6m },
+                    { 4, "Cenoura", 90m, 4m },
+                    { 5, "Alface", 30m, 3m },
+                    { 6, "Uva", 200m, 15m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
